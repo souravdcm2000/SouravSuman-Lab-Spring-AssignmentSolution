5d20c951b01fb712a69cb72b6d4832e9b15addd7
souravdcm2000 <42172921+souravdcm2000@users.noreply.github.com>
2022-05-28 10:46:26 +0530

    Update StudentManagementBootApplicationTests.java

--- SouravSuman-Lab-Spring-AssignmentSolution/src/test/java/com/sourav/StudentManagementBoot/StudentManagementBootApplicationTests.java
+++ SouravSuman-Lab-Spring-AssignmentSolution/src/test/java/com/sourav/StudentManagementBoot/StudentManagementBootApplicationTests.java
@@ -1,4 +1,4 @@
-package com.shaurya.StudentManagementBoot;
+package com.sourav.StudentManagementBoot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
